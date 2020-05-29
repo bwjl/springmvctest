57dc5be5980232af370a60c1595561eb36ecb7ec
bear <593788753@qq.com>
2020-05-29 11:35:19 +0800

    bean xml 自动装配（注入）

---
+++ src/main/java/com/bear/springmvctest/component/School.java
@@ -0,0 +1,21 @@
+package com.bear.springmvctest.component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/5/29 上午11:19
+ * Description:
+ */
+
+public class School {
+
+    private PersonInfo personInfo;
+
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+}
---
+++ src/main/java/com/bear/springmvctest/controller/AutoWiredPropertyController.java
@@ -0,0 +1,40 @@
+package com.bear.springmvctest.controller;
+
+import com.bear.springmvctest.component.PersonInfo;
+import com.bear.springmvctest.component.School;
+import com.bear.springmvctest.entity.Order;
+import com.bear.springmvctest.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/5/29 上午10:38
+ * Description: Bean 自动装配属性
+ */
+@Controller
+@RequestMapping("/autoWiredPropertyController")
+public class AutoWiredPropertyController {
+
+    /**
+     * 由bean xml 自动装配（注入）
+     */
+    //private PersonService personService;
+    //private PersonInfo personInfo;
+    private Order order;
+
+    @Autowired
+    private School school;
+
+    @RequestMapping("/index")
+    public void index() {
+        System.out.println("autoWiredPropertyController:index");
+        //System.out.println(personService);
+        //System.out.println(personInfo);
+        //System.out.println(order);
+        System.out.println(school.getPersonInfo());
+
+    }
+}
