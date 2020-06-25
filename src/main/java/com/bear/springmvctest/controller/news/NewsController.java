94ddefbda73bb0b2aef6a1c56d36eea9c59d77af
bear <593788753@qq.com>
2020-06-25 17:11:03 +0800

    idea Put into Output Root SpringMVC4.x-->SpringMvc5.x

--- src/main/java/com/bear/springmvctest/controller/news/NewsController.java
+++ src/main/java/com/bear/springmvctest/controller/news/NewsController.java
@@ -2,6 +2,7 @@ package com.bear.springmvctest.controller.news;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
@@ -18,7 +19,7 @@ import java.util.Map;
 @RequestMapping("/news")
 public class NewsController {
 
-    @RequestMapping("/index")
+    @GetMapping("/index")
     public String index(Model model) {
         String tips = "my first JSP page";
 //        Map<String , String> tips = new HashMap<String, String>();
--- webapp/index.jsp
+++ webapp/index.jsp
@@ -8,9 +8,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>$Title$</title>
+    <title>welcome pages</title>
 </head>
 <body>
-$END$
+welcome pages
 </body>
 </html>
