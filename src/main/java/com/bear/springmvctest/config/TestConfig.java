0517ba310fb9c8360e5123d4c16955d6a757dff8
bear <593788753@qq.com>
2020-07-27 17:24:13 +0800

    spring mvc 关闭spring security csrf验证

---
+++ src/main/java/com/bear/springmvctest/config/TestConfig.java
@@ -0,0 +1,18 @@
+package com.bear.springmvctest.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/7/27 下午4:03
+ * Description:
+ */
+
+@Configuration
+public class TestConfig {
+
+    public TestConfig() {
+        System.out.println("TestConfig");
+    }
+}
---
+++ src/main/java/com/bear/springmvctest/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.bear.springmvctest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/7/27 下午3:43
+ * Description: WebSecurityConfig
+ */
+
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    public void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.csrf().disable(); //关闭csrf保护
+//    }
+//}
+
+public class WebSecurityConfig {
+
+}
