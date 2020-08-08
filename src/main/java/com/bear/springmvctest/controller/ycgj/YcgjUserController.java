c3eadf94d79a2a19c7208415712e95a3974bd7b5
bear <593788753@qq.com>
2020-08-08 17:58:44 +0800

    初步奖springmvc集成mybatis

---
+++ src/main/java/com/bear/springmvctest/controller/ycgj/YcgjUserController.java
@@ -0,0 +1,40 @@
+package com.bear.springmvctest.controller.ycgj;
+
+import com.bear.springmvctest.entityYcgj.User;
+import com.bear.springmvctest.service.UserService;
+import com.bear.springmvctest.util.JsonUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/8/6 上午10:59
+ * Description:
+ */
+
+@Controller
+@RequestMapping("ycgj/user")
+public class YcgjUserController {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("getList")
+    public void getList() {
+
+    }
+
+    @GetMapping("queryById")
+    public String queryById(@RequestParam("id") Integer id) throws JsonProcessingException {
+        User user = userService.queryById(id);
+        return JsonUtil.obj2Json(user);
+
+    }
+}
---
+++ src/main/java/com/bear/springmvctest/util/JsonUtil.java
@@ -0,0 +1,26 @@
+package com.bear.springmvctest.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/8/8 上午11:28
+ * Description:
+ */
+
+public class JsonUtil {
+
+    /**
+     * 对象转json字符串
+     *
+     * @param object
+     * @return
+     * @throws JsonProcessingException
+     */
+    public static String obj2Json(Object object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(object);
+    }
+}
