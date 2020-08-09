8b8fa5a281ac2283e8ca856567aa7c2238867811
bear <593788753@qq.com>
2020-08-09 16:51:03 +0800

    多个写法：<context:property-placeholder location="classpath:jdbc.properties,classpath:application.properties"/>

--- src/main/java/com/bear/springmvctest/controller/SpELController.java
+++ src/main/java/com/bear/springmvctest/controller/SpELController.java
@@ -16,6 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("SpEL")
 public class SpELController {
 
+    @Value("${jdbc.user}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
+
     @Value("#{order.orderSn}") //获取bean中属性
     private String prefix;
 
@@ -25,11 +31,15 @@ public class SpELController {
     @Value("${springframework.version}") //读取application.properties配置
     private String springFrameworkVersion;
 
+
     @GetMapping("index")
     public void index() {
         System.out.println(name); //bear
         System.out.println(prefix); //20200528001
         System.out.println(springFrameworkVersion); //bear
+
+        System.out.println("数据库用户名：" + username);
+        System.out.println("数据库密码：" + password);
 
     }
 
