98bdb53f246f19d8f90984e8ce7b6538401058c6
bear <593788753@qq.com>
2020-12-01 23:22:16 +0800

    idea Command+N 快捷键生成getter setter 和 toString

---
+++ src/main/java/com/bear/springmvctest/entity/People.java
@@ -0,0 +1,39 @@
+package com.bear.springmvctest.entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/12/1 下午11:13
+ * Description:
+ */
+
+public class People {
+
+    private String name;
+
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
