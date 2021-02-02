dc86bbd76805c68e2d85efa574fbd56cd560febf
bear <593788753@qq.com>
2021-02-02 11:42:36 +0800

    SpringMVC aop 拦截service方法 BeanWrapper 设置属性

--- src/main/java/com/bear/springmvctest/common/aop/DalAop.java
+++ src/main/java/com/bear/springmvctest/common/aop/DalAop.java
@@ -1,6 +1,8 @@
 package com.bear.springmvctest.common.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,10 +13,8 @@ import org.aspectj.lang.JoinPoint;
 
 public class DalAop {
 
-    private static final String CREATOR = "creator";
-    private static final String MODIFIER = "modifier";
-    private static final String GMTCREATE = "gmtCreate";
-    private static final String GMTMODIFIED = "gmtModified";
+    private static final String CREATE_TIME = "createTime";
+    private static final String UPDATE_TIME = "updateTime";
 
     public void beforeInsert(JoinPoint joinPoint) {
 
@@ -23,7 +23,20 @@ public class DalAop {
         Object[] args = joinPoint.getArgs();
 
         if (args != null && args.length > 0) {
+            Object arg = args[0];
+            BeanWrapper beanWrapper = new BeanWrapperImpl(arg);
+            if (beanWrapper.isWritableProperty(CREATE_TIME)) {
+                beanWrapper.setPropertyValue(CREATE_TIME, (int) (System.currentTimeMillis() / 1000L));
+            }
 
+            if (beanWrapper.isWritableProperty(UPDATE_TIME)) {
+                beanWrapper.setPropertyValue(UPDATE_TIME, (int) (System.currentTimeMillis() / 1000L));
+            }
+
+            if (beanWrapper.isWritableProperty("xxx")) {
+
+                System.out.println("xxx");
+            }
         }
 
     }
